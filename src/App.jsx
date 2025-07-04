41281f8a3e1eb5bddf6afa2fde282bfc4882e12b
emisanzmor <emisn1501@gmail.com>
2025-07-03 22:36:44 -0600

    Refactor: split landing page into multiple pages and configure routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
+
+import Header from "./components/Header";
 import About from "./components/About";
 import Footer from "./components/Footer";
 import LoadingScreen from "./components/LoadingScreen";
+import ProjectsPage from "./pages/ProjectsPage";
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
@@ -38,14 +41,17 @@ function App() {
         minHeight: "100vh",
       }}
     >
-      <LoadingScreen isVisible={isLoading} />
-      <Header /> {/* Header section */}
-      <main>
-        <Hero /> {/* Hero section */}
-        <Projects /> {/* Projects section */}
-        <About /> {/* About section */}
-      </main>
-      <Footer /> {/* Footer Section */}
+      <Router>
+        <LoadingScreen isVisible={isLoading} />
+        <Header /> {/* Header section */}
+        <main>
+          <Routes>
+            <Route path="/" element={<About />} />
+            <Route path="/projects" element={<ProjectsPage />} />
+          </Routes>
+        </main>
+        <Footer /> {/* Footer Section */}
+      </Router>
     </div>
   );
 }
